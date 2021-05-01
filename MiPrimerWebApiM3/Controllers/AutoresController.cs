005fa3fa864e5ebc5b3906672fb9b3fedbe36bec
OmarBello <72562209+OmarBello@users.noreply.github.com>
2021-05-01 08:49:39 -0400

    Subiendo Primer Proyecto

---
+++ MiPrimerWebApiM3/Controllers/AutoresController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiPrimerWebApiM3.Contexts;
+using MiPrimerWebApiM3.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApiM3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.Include(x => x.Libros).ToList(); 
+        }
+
+        [HttpGet("{id}", Name = "obtenerAutor")]
+        public ActionResult<Autor> Get(int id)
+        {
+            var autor = context.Autores.Include(x => x.Libros).FirstOrDefault(x => x.Id == id);
+            if(autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Autor autor)
+        {
+            //if (!modelstate.isvalid)
+            //{
+            //    return badrequest(modelstate);
+            //}
+
+            context.Autores.Add(autor);
+            context.SaveChanges();
+            return new CreatedAtRouteResult("obtenerAutor", new { id = autor.Id }, autor);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Autor value)
+        {
+            //if (!modelstate.isvalid)
+            //{
+            //    return badrequest(modelstate);
+            //}
+
+            if (id != value.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(value).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Autor> Delete(int id)
+        {
+            var autor = context.Autores.FirstOrDefault(x => x.Id == id);
+            context.SaveChanges();
+            return autor;
+        }
+    }
+}
